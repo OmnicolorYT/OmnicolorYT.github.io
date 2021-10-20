6c10bcd8e58c7724dfd2f92251307b8b6127d045
OmnicolorYT <omnicolor.prog@gmail.com>
2021-10-20 23:53:47 +0300

    Less, gulp, header
    Установлен препроцессор less, настроен сборщик gulp.
    Переделана файловая структура проекта.
    Прописаны стили header, отредактирована разметка header.

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+const gulp = require('gulp');
+const less = require('gulp-less');
+const concat = require('gulp-concat');
+const src_path = 'src/less/*.less';
+
+gulp.task('less_constructor', function() {
+    return gulp.src(src_path)
+        .pipe(less())
+        .pipe(concat('style.css'))
+        .pipe(gulp.dest('build/styles'))
+})
+
+gulp.task('watch', function(){
+    return gulp.watch(src_path, gulp.series('less_constructor'));
+})
+
+gulp.task('default', gulp.series('less_constructor', 'watch'));
